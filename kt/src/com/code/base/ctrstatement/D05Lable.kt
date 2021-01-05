a7aa81c64117a439059ed626e038a3c64573b2c5
i9code <i9code@aliyun.com>
2021-01-06 00:46:17 +0800

    ---

--- kt/src/com/code/base/ctrstatement/D05Lable.kt
+++ kt/src/com/code/base/ctrstatement/D05Lable.kt
@@ -1,14 +1,14 @@
 package com.code.base.ctrstatement
 
 fun main(args: Array<String>) {
-    // 定义标签
-    loop@ for (i in 1..4) {
-        for (j in 1..4) {
-            if (i == 2 && j == 2) {
-                // break到指定标签
-                break@loop
-            }
-            println("i = $i, j = $j")
-        }
-    }
+	// 定义标签
+	loop@ for (i in 1..4) {
+		for (j in 1..4) {
+			if (i == 2 && j == 2) {
+				// break到指定标签
+				break@loop
+			}
+			println("i = $i, j = $j")
+		}
+	}
 }
